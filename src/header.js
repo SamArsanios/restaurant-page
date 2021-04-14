1502aa3b5dba259a2fd19101ae58d231206f291e
SamArsanios <samson.kibrom@hotmail.com>
2021-04-14 20:23:16 +0300

    Add header method

--- src/header.js
+++ src/header.js
@@ -1,22 +1,15 @@
-const content = document.querySelector('#content');
-
 const header = () => {
-    const header = document.createElement('header');
-    const nav = document.createElement('nav');
+  
+    const content = document.querySelector('#header');
+    const nav = document.createElement('div');
+    
     nav.classList.add('fixed', 'topnav');
     const restaurant = document.createElement('h1');
-    restaurant.textContent = 'Arsan\'s Diner';
+    restaurant.textContent = 'Arsani\'s Diner';
     restaurant.classList.add('arsani');
 
+    content.appendChild(nav);
     nav.appendChild(restaurant);
-    header.appendChild(nav);
-    content.appendChild(header);
 };
 
 export default header;
-
-// .center {
-//     text-align: center;
-//     padding-top: 20px;
-//     padding-bottom: 20px;
-// }
