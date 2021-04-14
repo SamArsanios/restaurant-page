22bb1bde0d6fb5bb60c69430dbf43bcf911e3184
SamArsanios <samson.kibrom@hotmail.com>
2021-04-14 20:24:05 +0300

    Add longer lorem

--- src/home.js
+++ src/home.js
@@ -5,7 +5,7 @@ const home = (content) => {
     header.textContent = 'Habesha Restaurant';
 
     const info = document.createElement('p');
-    info.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed accusantium possimus iste aperiam deserunt ut maxime nisi, molestiae natus provident quisquam fugiat quaerat repudiandae facere nesciunt repellat magnam placeat sunt.';
+    info.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptatibus voluptas modi corrupti deserunt quisquam vitae eos facilis perferendis sequi saepe.Totam nemo vitae et commodi recusandae officiis fugit labore error quidem, praesentium adipisci explicabo veniam non sed, aspernatur fuga eius reiciendis inventore vel minus quis quibusdam architecto! Ducimus, amet ut!';
     home.appendChild(header);
     home.appendChild(info);
     content.appendChild(home);
